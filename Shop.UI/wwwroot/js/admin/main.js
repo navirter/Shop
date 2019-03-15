78adb1b0a615abf9afcdb5c39d4cf5a21c736105
Valeriy Sedov <navirter@gmail.com>
2019-03-15 12:05:23 +0300

    Implemented Vue. Implemented an input and reactive p. Made them hide or dissapear in accordance to the state changing button click.

---
+++ Shop.UI/wwwroot/js/admin/main.js
@@ -0,0 +1,17 @@
+﻿var app = new Vue({
+    el: '#app',
+    data:    {
+        price: 0,
+        showPrice: true
+    },
+    methods: {
+        togglePrice: function () {
+            this.showPrice = !this.showPrice;
+        }
+    },
+    computed:    {
+        formatPrice: function() {
+            return this.price + "$";
+        }
+    }
+});
