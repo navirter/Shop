4d207273b21097e183717b34b4c1b2b75a4dd1a0
Valeriy Sedov <navirter@gmail.com>
2019-03-15 13:58:01 +0300

    Implemented edition and removing features for admin. Previous unided items in db cause some nasty errors.

--- Shop.Application/ProductsAdmin/UpdateProduct.cs
+++ Shop.Application/ProductsAdmin/UpdateProduct.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace Shop.Application.ProductsAdmin
 {
@@ -18,12 +19,25 @@ namespace Shop.Application.ProductsAdmin
 
         public async Task<Response> Do(Request request)
         {
+            var product = _context.Products.FirstOrDefault(x => x.Id == request.Id);
+
+            product.Name = request.Name;
+            product.Description = request.Description;
+            product.Value = request.Value;
+
             await _context.SaveChangesAsync();
-            return new Response();
+            return new Response()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Value = product.Value
+            };
         }
 
         public class Request
         {
+            public int Id { get; set; }
             public string Name { get; set; }
             public string Description { get; set; }
             public decimal Value { get; set; }
--- Shop.UI/wwwroot/js/admin/main.js
+++ Shop.UI/wwwroot/js/admin/main.js
@@ -2,7 +2,9 @@
     el: '#app',
     data: {
         loading: false,
+        objectIndex: 0,
         productModel: {
+            id: 0,
             name: "Product Name",
             description: "Product Description",
             value: 1.99
@@ -10,6 +12,20 @@
         products: []
     },
     methods: {
+        getProduct: function (id) {
+            this.loading = true;
+            axios.get('/Admin/product/'+id)
+                .then(res => {
+                    console.log(res);
+                    this.products = res.data;
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
         getProducts: function () {
             this.loading = true;
             axios.get('/Admin/products')
@@ -37,6 +53,43 @@
                 .then(() => {
                     this.loading = false;
                 });
+        },
+        updateProduct: function () {
+            this.loading = true;
+            axios.put('/Admin/products', this.productModel)
+                .then(res => {
+                    console.log(res.data);
+                    this.products.splice(this.objectIndex, 1, res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+        deleteProduct: function (id, index) {
+            this.loading = true;
+            axios.delete('/Admin/products/' + id)
+                .then(res => {
+                    console.log(res);
+                    this.products.splice(index, 1);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+        editProduct: function (product, index) {
+            this.objectIndex = index;
+            this.productModel = {                
+                id: product.id,
+                name: product.name,
+                description: product.description,
+                value: product.value
+            };
         }
     },
     computed: {
