eea7662bdd275b777199af2776a37ddb584944bc
Valeriy Sedov <navirter@gmail.com>
2019-03-15 10:50:30 +0300

    Added Orders, Stock and OrderStock. Also added mappings and user ORM to renew DB

---
+++ Shop.Application/Products/CreateProduct.cs
@@ -0,0 +1,31 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Products
+{
+    public class CreateProduct
+    {
+        private ApplicationDbContext _context;
+
+        public CreateProduct(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Do(string name, string description, decimal value)
+        {
+            _context.Products.Add(new Product()
+            {
+                Name = name, 
+                Description = description,
+                Value = value
+            });
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Shop.UI/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shop.Application.Products;
+using Shop.Database;
+
+namespace Shop.UI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public ProductViewModel Product{ get; set; }
+
+        public class ProductViewModel
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await new CreateProduct(_context).Do(Product.Name, Product.Description, Product.Value);
+
+            return RedirectToPage("Index");
+        }
+    }
+}
