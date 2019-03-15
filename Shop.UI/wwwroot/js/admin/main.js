cf6232f40ec6fd2c29bc19bae6cdeea0e336a567
Valeriy Sedov <navirter@gmail.com>
2019-03-15 13:20:46 +0300

    Implemented get and create admin functionality. Displaying is misfunctioning though.

--- Shop.UI/wwwroot/js/admin/main.js
+++ Shop.UI/wwwroot/js/admin/main.js
@@ -1,17 +1,45 @@
 ﻿var app = new Vue({
     el: '#app',
-    data:    {
-        price: 0,
-        showPrice: true
+    data: {
+        loading: false,
+        productModel: {
+            name: "Product Name",
+            description: "Product Description",
+            value: 1.99
+        },
+        products: []
     },
     methods: {
-        togglePrice: function () {
-            this.showPrice = !this.showPrice;
+        getProducts: function () {
+            this.loading = true;
+            axios.get('/Admin/products')
+                .then(res => {
+                    console.log(res);
+                    this.products = res.data;
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+        createProduct: function () {
+            this.loading = true;
+            axios.post('/Admin/products', this.productModel)
+                .then(res => {
+                    console.log(res.data);
+                    this.products.push(res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
         }
     },
-    computed:    {
-        formatPrice: function() {
-            return this.price + "$";
-        }
+    computed: {
+
     }
 });
