2b7eb547034fb850691e7b860cd6384d38fdd199
Valeriy Sedov <navirter@gmail.com>
2019-03-18 14:10:09 +0300

    Created CRUD operations for Stock class at the application layer.

---
+++ Shop.Application/StockAdmin/DeleteStock.cs
@@ -0,0 +1,42 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Shop.Application.StockAdmin
+{
+    public class DeleteStock
+    {
+        private ApplicationDbContext _context;
+
+        public DeleteStock(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Do(int id)
+        {
+            var stock = _context.Stock.FirstOrDefault(x => x.Id == id);
+            _context.Stock.Remove(stock);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public class Request
+        {
+            public int ProductId { get; set; }
+            public string Description { get; set; }
+            public int Qty { get; set; }
+        }
+
+        public class Response
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public int Qty { get; set; }
+        }
+    }
+}
