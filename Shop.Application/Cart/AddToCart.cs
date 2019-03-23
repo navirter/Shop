246f5e570b7ede60bbe03ae6a12bbc4f25e288e0
Navirter <navirter@gmail.com>
2019-03-23 17:02:32 +0300

    Implemented a simple shopping cart. Made redirection to the cat page after buying staff

---
+++ Shop.Application/Cart/AddToCart.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Application.Cart
+{
+    public class AddToCart
+    {
+        private ISession _session;
+
+        public AddToCart(ISession session)
+        {
+            _session = session;
+        }
+
+        public class Request
+        {
+            public int StockId{ get; set; }
+            public int Qty { get; set; }
+        }
+
+        public class Response
+        {
+
+        }
+
+        public void Do(Request request)
+        {
+            var cartProduct = new CartProduct
+            {
+                StockId = request.StockId,
+                Qty = request.Qty
+            };
+            string str = JsonConvert.SerializeObject(cartProduct);
+
+            //TODO: append the cart
+
+            _session.SetString("cart", str);
+        }
+    }
+}
---
+++ Shop.Application/Cart/GetCart.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.Application.Cart
+{
+    public  class GetCart
+    {
+        private ISession _session;
+        private ApplicationDbContext _context;
+
+        public GetCart(ISession session, ApplicationDbContext context)
+        {
+            _session = session;
+            _context = context;
+        }
+
+        public class Response
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+            public int StockId { get; set; }
+            public int Qty { get; set; }
+        }
+
+        public Response Do()
+        {
+            //TODO: account for multiple items in the cart
+            string str = _session.GetString("cart");
+            var cartProduct = JsonConvert.DeserializeObject<CartProduct>(str);
+            var response = _context.Stock
+                .Include(x => x.Product)
+                .Where(x => x.Id == cartProduct.StockId)
+                .Select(x => new Response
+                {
+                    Name = x.Product.Name,
+                    Value = x.Product.Value.ToString("N2") + "$",
+                    StockId = x.Id,
+                    Qty = cartProduct.Qty
+                })
+                .FirstOrDefault();
+            return response;
+        }
+    }
+}
