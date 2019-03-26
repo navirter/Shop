d212b12b4792080e93879646c0ba85aa00335489
Valeriy Sedov <navirter@gmail.com>
2019-03-26 13:07:18 +0300

    Implemented CartViewComponenet. Failed to move out Componenets folder from Views\Shared

---
+++ Shop.UI/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Cart;
+using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.UI.ViewComponents
+{
+    /*this is what should be used for reusable components throughout the web app*/
+    public class CartViewComponent : ViewComponent
+    {
+        private ApplicationDbContext _context;
+
+        public CartViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke(string view ="Default")
+        {
+            var cart = new GetCart(HttpContext.Session, _context).Do();
+            return View(view, cart);
+        }
+    }
+}
