c6a383db8b8bc637135717a46e98f7e2be49e03c
Valeriy Sedov <navirter@gmail.com>
2019-03-31 13:48:55 +0300

    TODO: fix cart component to show properly. Implemented saving orders

--- Shop.UI/Pages/Checkout/CustomerInformation.cshtml.cs
+++ Shop.UI/Pages/Checkout/CustomerInformation.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Shop.Application.Cart;
@@ -10,6 +11,12 @@ namespace Shop.UI.Pages.Checkout
 {
     public class CustomerInformationModel : PageModel
     {
+        private IHostingEnvironment _env;
+
+        public CustomerInformationModel(IHostingEnvironment env)
+        {
+            _env = env;
+        }
 
         [BindProperty]
         public AddCustomerInformation.Request CustomerInformation { get; set; }
@@ -20,7 +27,23 @@ namespace Shop.UI.Pages.Checkout
             var information = new GetCustomerInformation(HttpContext.Session).Do();
             //if cart exists go to payment
             if (information == null)
+            {
+                if (_env.IsDevelopment())
+                {
+                    CustomerInformation = new AddCustomerInformation.Request
+                    {
+                        FirstName = "a",
+                        LastName = "a",
+                        Email = "a@a.com",
+                        PhoneNumber = "11",
+                        Address1 = "a",
+                        Address2 = "a",
+                        City = "a",
+                        PostCode = "a"
+                    };
+                }
                 return Page();
+            }
             else
                 return RedirectToPage("/Checkout/Payment");
         }
--- Shop.UI/Pages/Checkout/Payment.cshtml.cs
+++ Shop.UI/Pages/Checkout/Payment.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using Shop.Application.Cart;
+using Shop.Application.Orders;
 using Shop.Database;
 using Stripe;
 
@@ -13,7 +14,7 @@ namespace Shop.UI.Pages.Checkout
 {
     public class PaymentModel : PageModel
     {
-
+        
         public string PublicKey { get; }
 
         private ApplicationDbContext _context;
@@ -32,7 +33,7 @@ namespace Shop.UI.Pages.Checkout
             return Page();
         }
 
-        public IActionResult OnPost(string stripeEmail, string stripeToken)
+        public async Task<IActionResult> OnPost(string stripeEmail, string stripeToken)
         {
             var customers = new CustomerService();
             var charges = new ChargeService();
@@ -52,6 +53,27 @@ namespace Shop.UI.Pages.Checkout
                 Currency = "usd",//gbp//rub
                 CustomerId = customer.Id
             });
+
+            await new CreateOrder(_context).Do(new CreateOrder.Request
+            {
+                StripeReference = charge.OrderId,
+
+                FirstName = CartOrder.CustomerInformation.FirstName,
+                LastName = CartOrder.CustomerInformation.LastName,
+                Email = CartOrder.CustomerInformation.Email,
+                PhoneNumber = CartOrder.CustomerInformation.PhoneNumber,
+                Address1 = CartOrder.CustomerInformation.Address1,
+                Address2 = CartOrder.CustomerInformation.Address2,
+                City = CartOrder.CustomerInformation.City,
+                PostCode = CartOrder.CustomerInformation.PostCode, 
+
+                Stocks = CartOrder.Products.Select(x=> new CreateOrder.Stock
+                {
+                    StockId = x.StockId,
+                    Qty = x.Qty
+                }).ToList()
+            });
+
 
             return RedirectToPage("/Index");
         }
