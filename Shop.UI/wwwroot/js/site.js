4283b3edd5e19f8178a537b1518962b28f216517
Valeriy Sedov <navirter@gmail.com>
2019-04-13 15:07:17 +0300

    Implemented Vue.js for Index.cshtml

--- Shop.UI/wwwroot/js/site.js
+++ Shop.UI/wwwroot/js/site.js
@@ -1,27 +1,49 @@
 ﻿var app = new Vue({
     el: '#app',
     data: {
-        menu: 0,
-        editing: false,
         loading: false,
-        objectIndex: 0,
-        productModel: {
-            id: 0,
-            name: "Product Name",
-            description: "Product Description",
-            value: 1.99,
-            category: ""
-        },
+        categories: [],
+        selectedCategory: "All",
         products: []
     },
     mounted() {
-        this.getProducts();
-
-    },
+        //this.getProducts();
+        this.getCategories();
+        this.selectCategory("All");
+    },//not sure if this runs at page loading as i expect it to
     methods: {
         getProducts: function () {
             this.loading = true;
             axios.get('/products')
+                .then(res => {
+                    console.log(res);
+                    this.products = res.data;
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+        getCategories: function () {
+            this.loading = true;
+            axios.get('/categories')
+                .then(res => {
+                    console.log(res);
+                    this.categories = res.data;
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+        selectCategory: function (category) {
+            this.selectedCategory = category;
+            this.loading = true;
+            axios.get('/categories/' + category)
                 .then(res => {
                     console.log(res);
                     this.products = res.data;
