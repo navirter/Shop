9d55e7b372624f73d19965c5fa27da48855f563e
Valeriy Sedov <navirter@gmail.com>
2019-04-13 15:42:52 +0300

    Added a base structure at index.cshtml using vue.js

--- Shop.UI/wwwroot/js/products.js
+++ Shop.UI/wwwroot/js/products.js
@@ -2,12 +2,18 @@
     el: '#app',
     data: {
         loading: false,
-        categories: [],
+        categories: [],//string[]
         selectedCategory: "All",
         products: []
+        /*
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Value { get; set; }
+            public string Category { get; set; }
+            public int StockCount { get; set; }
+        */
     },
-    beforeMount() {
-        //this.getProducts();
+    beforeMount() {        
         this.getCategories();
         this.selectCategory("All");
     },//not sure if this runs at page loading as i expect it to
