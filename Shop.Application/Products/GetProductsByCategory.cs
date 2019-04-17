6e11fcde15c6d908fc5cf46c3982998575620606
Valeriy Sedov <navirter@gmail.com>
2019-04-17 15:36:02 +0300

    UI: Elemenet's size doen't affect alignment

--- Shop.Application/Products/GetProductsByCategory.cs
+++ Shop.Application/Products/GetProductsByCategory.cs
@@ -26,6 +26,7 @@ namespace Shop.Application.Products
               {
                   Name = s.Name,
                   Description = s.Description,
+                  DescriptionShort = s.Description.Substring(0, 30) + "...",
                   Value = s.Value.ToString("N2") + "$",  //1100.50 => 1,100.50 
                   StockCount = s.Stock.Sum(x => x.Qty),
                   Category = s.Category
@@ -46,6 +47,7 @@ namespace Shop.Application.Products
         {
             public string Name { get; set; }
             public string Description { get; set; }
+            public string DescriptionShort { get; set; }
             public string Value { get; set; }
             public string Category { get; set; }
             public int StockCount { get; set; }
